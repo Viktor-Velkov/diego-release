cloudfoundry/rep

Namespace auction things with LRP in preparation for task auctions [#83638690]
Signed-off-by: Eric Malm <emalm@pivotal.io>

auction_cell_rep/auction_cell_rep.go
@@ -87,24 +87,24 @@ func (a *AuctionCellRep) Perform(work auctiontypes.Work) (auctiontypes.Work, err
 	var failedWork = auctiontypes.Work{}
 
 	logger := a.logger.Session("auction-work", lager.Data{
-		"starts": len(work.Starts),
-		"stops":  len(work.Stops),
+		"lrp-starts": len(work.LRPStarts),
+		"lrp-stops":  len(work.LRPStops),
 	})
 
-	for _, stop := range work.Stops {
-		stopLogger := logger.Session("stop-instance", lager.Data{"process-guid": stop.ProcessGuid, "instance-guid": stop.InstanceGuid, "index": stop.Index})
+	for _, stop := range work.LRPStops {
+		stopLogger := logger.Session("lrp-stop-instance", lager.Data{"process-guid": stop.ProcessGuid, "instance-guid": stop.InstanceGuid, "index": stop.Index})
 		stopLogger.Info("stopping")
-		err := a.stop(stop)
+		err := a.stopLRP(stop)
 		if err != nil {
 			stopLogger.Error("failed-to-stop", err)
-			failedWork.Stops = append(failedWork.Stops, stop)
+			failedWork.LRPStops = append(failedWork.LRPStops, stop)
 		} else {
 			stopLogger.Info("stopped")
 		}
 	}
 
-	for _, start := range work.Starts {
-		startLogger := logger.Session("start-instance", lager.Data{
+	for _, start := range work.LRPStarts {
+		startLogger := logger.Session("lrp-start-instance", lager.Data{
 			"process-guid":  start.DesiredLRP.ProcessGuid,
 			"instance-guid": start.InstanceGuid,
 			"index":         start.Index,
@@ -112,10 +112,10 @@ func (a *AuctionCellRep) Perform(work auctiontypes.Work) (auctiontypes.Work, err
 			"disk-mb":       start.DesiredLRP.DiskMB,
 		})
 		startLogger.Info("starting")
-		err := a.start(start, startLogger)
+		err := a.startLRP(start, startLogger)
 		if err != nil {
 			startLogger.Error("failed-to-start", err)
-			failedWork.Starts = append(failedWork.Starts, start)
+			failedWork.LRPStarts = append(failedWork.LRPStarts, start)
 		} else {
 			startLogger.Info("started")
 		}
@@ -124,7 +124,7 @@ func (a *AuctionCellRep) Perform(work auctiontypes.Work) (auctiontypes.Work, err
 	return failedWork, nil
 }
 
-func (a *AuctionCellRep) start(startAuction models.LRPStartAuction, logger lager.Logger) error {
+func (a *AuctionCellRep) startLRP(startAuction models.LRPStartAuction, logger lager.Logger) error {
 	logger.Info("reserving")
 
 	containerGuid := startAuction.InstanceGuid
@@ -184,7 +184,7 @@ func (a *AuctionCellRep) start(startAuction models.LRPStartAuction, logger lager
 	return nil
 }
 
-func (a *AuctionCellRep) stop(stopInstance models.StopLRPInstance) error {
+func (a *AuctionCellRep) stopLRP(stopInstance models.StopLRPInstance) error {
 	return a.lrpStopper.StopInstance(stopInstance)
 }
 

auction_cell_rep/auction_cell_rep_test.go
@@ -189,9 +189,9 @@ var _ = Describe("AuctionCellRep", func() {
 				Index:        2,
 			}
 
-			work.Starts = []models.LRPStartAuction{startAuction}
+			work.LRPStarts = []models.LRPStartAuction{startAuction}
 
-			work.Stops = []models.StopLRPInstance{stopInstance}
+			work.LRPStops = []models.StopLRPInstance{stopInstance}
 		})
 
 		Describe("performing starts", func() {
@@ -258,7 +258,7 @@ var _ = Describe("AuctionCellRep", func() {
 				It("should mark the start as failed", func() {
 					failedWork, err := cellRep.Perform(work)
 					Ω(err).ShouldNot(HaveOccurred(), "note: we don't error")
-					Ω(failedWork.Starts).Should(ConsistOf(startAuction))
+					Ω(failedWork.LRPStarts).Should(ConsistOf(startAuction))
 				})
 
 				It("should not report to the BBS, or try to run the container", func() {
@@ -276,7 +276,7 @@ var _ = Describe("AuctionCellRep", func() {
 				It("should mark the start as failed", func() {
 					failedWork, err := cellRep.Perform(work)
 					Ω(err).ShouldNot(HaveOccurred(), "note: we don't error")
-					Ω(failedWork.Starts).Should(ConsistOf(startAuction))
+					Ω(failedWork.LRPStarts).Should(ConsistOf(startAuction))
 				})
 
 				It("should delete the container and not try to run the container", func() {
@@ -295,7 +295,7 @@ var _ = Describe("AuctionCellRep", func() {
 				It("should mark the start as failed", func() {
 					failedWork, err := cellRep.Perform(work)
 					Ω(err).ShouldNot(HaveOccurred(), "note: we don't error")
-					Ω(failedWork.Starts).Should(ConsistOf(startAuction))
+					Ω(failedWork.LRPStarts).Should(ConsistOf(startAuction))
 				})
 
 				It("should delete the container and remove the Actual from the BBS", func() {
@@ -325,7 +325,7 @@ var _ = Describe("AuctionCellRep", func() {
 				It("should mark the stop as failed", func() {
 					failedWork, err := cellRep.Perform(work)
 					Ω(err).ShouldNot(HaveOccurred(), "note: we don't error")
-					Ω(failedWork.Stops).Should(ConsistOf(stopInstance))
+					Ω(failedWork.LRPStops).Should(ConsistOf(stopInstance))
 				})
 			})
 		})

Viktor-Velkov/diego-release

src/code.cloudfoundry.org/rep/auction_cell_rep/auction_cell_rep.go
@@ -87,24 +87,24 @@ func (a *AuctionCellRep) Perform(work auctiontypes.Work) (auctiontypes.Work, err
 	var failedWork = auctiontypes.Work{}
 
 	logger := a.logger.Session("auction-work", lager.Data{
-		"starts": len(work.Starts),
-		"stops":  len(work.Stops),
+		"lrp-starts": len(work.LRPStarts),
+		"lrp-stops":  len(work.LRPStops),
 	})
 
-	for _, stop := range work.Stops {
-		stopLogger := logger.Session("stop-instance", lager.Data{"process-guid": stop.ProcessGuid, "instance-guid": stop.InstanceGuid, "index": stop.Index})
+	for _, stop := range work.LRPStops {
+		stopLogger := logger.Session("lrp-stop-instance", lager.Data{"process-guid": stop.ProcessGuid, "instance-guid": stop.InstanceGuid, "index": stop.Index})
 		stopLogger.Info("stopping")
-		err := a.stop(stop)
+		err := a.stopLRP(stop)
 		if err != nil {
 			stopLogger.Error("failed-to-stop", err)
-			failedWork.Stops = append(failedWork.Stops, stop)
+			failedWork.LRPStops = append(failedWork.LRPStops, stop)
 		} else {
 			stopLogger.Info("stopped")
 		}
 	}
 
-	for _, start := range work.Starts {
-		startLogger := logger.Session("start-instance", lager.Data{
+	for _, start := range work.LRPStarts {
+		startLogger := logger.Session("lrp-start-instance", lager.Data{
 			"process-guid":  start.DesiredLRP.ProcessGuid,
 			"instance-guid": start.InstanceGuid,
 			"index":         start.Index,
@@ -112,10 +112,10 @@ func (a *AuctionCellRep) Perform(work auctiontypes.Work) (auctiontypes.Work, err
 			"disk-mb":       start.DesiredLRP.DiskMB,
 		})
 		startLogger.Info("starting")
-		err := a.start(start, startLogger)
+		err := a.startLRP(start, startLogger)
 		if err != nil {
 			startLogger.Error("failed-to-start", err)
-			failedWork.Starts = append(failedWork.Starts, start)
+			failedWork.LRPStarts = append(failedWork.LRPStarts, start)
 		} else {
 			startLogger.Info("started")
 		}
@@ -124,7 +124,7 @@ func (a *AuctionCellRep) Perform(work auctiontypes.Work) (auctiontypes.Work, err
 	return failedWork, nil
 }
 
-func (a *AuctionCellRep) start(startAuction models.LRPStartAuction, logger lager.Logger) error {
+func (a *AuctionCellRep) startLRP(startAuction models.LRPStartAuction, logger lager.Logger) error {
 	logger.Info("reserving")
 
 	containerGuid := startAuction.InstanceGuid
@@ -188,7 +188,7 @@ func (a *AuctionCellRep) start(startAuction models.LRPStartAuction, logger lager
 	return nil
 }
 
-func (a *AuctionCellRep) stop(lrp models.ActualLRP) error {
+func (a *AuctionCellRep) stopLRP(lrp models.ActualLRP) error {
 	return a.lrpStopper.StopInstance(lrp)
 }
 

src/code.cloudfoundry.org/rep/auction_cell_rep/auction_cell_rep_test.go
@@ -196,10 +196,9 @@ var _ = Describe("AuctionCellRep", func() {
 				CellID: "some-cell-id",
 			}
 
-			work.Starts = []models.LRPStartAuction{startAuction}
+			work.LRPStarts = []models.LRPStartAuction{startAuction}
 
-			work.Stops = []models.ActualLRP{stoppingLRP}
-			work.Stops = []models.ActualLRP{stoppingLRP}
+			work.LRPStops = []models.ActualLRP{stoppingLRP}
 		})
 
 		Describe("performing starts", func() {
@@ -262,7 +261,7 @@ var _ = Describe("AuctionCellRep", func() {
 				It("should mark the start as failed", func() {
 					failedWork, err := cellRep.Perform(work)
 					Ω(err).ShouldNot(HaveOccurred(), "note: we don't error")
-					Ω(failedWork.Starts).Should(ConsistOf(startAuction))
+					Ω(failedWork.LRPStarts).Should(ConsistOf(startAuction))
 				})
 
 				It("should not report to the BBS, or try to run the container", func() {
@@ -280,7 +279,7 @@ var _ = Describe("AuctionCellRep", func() {
 				It("should mark the start as failed", func() {
 					failedWork, err := cellRep.Perform(work)
 					Ω(err).ShouldNot(HaveOccurred(), "note: we don't error")
-					Ω(failedWork.Starts).Should(ConsistOf(startAuction))
+					Ω(failedWork.LRPStarts).Should(ConsistOf(startAuction))
 				})
 
 				It("should delete the container and not try to run the container", func() {
@@ -299,7 +298,7 @@ var _ = Describe("AuctionCellRep", func() {
 				It("should mark the start as failed", func() {
 					failedWork, err := cellRep.Perform(work)
 					Ω(err).ShouldNot(HaveOccurred(), "note: we don't error")
-					Ω(failedWork.Starts).Should(ConsistOf(startAuction))
+					Ω(failedWork.LRPStarts).Should(ConsistOf(startAuction))
 				})
 
 				It("should delete the container and remove the Actual from the BBS", func() {
@@ -330,8 +329,7 @@ var _ = Describe("AuctionCellRep", func() {
 				It("should mark the stop as failed", func() {
 					failedWork, err := cellRep.Perform(work)
 					Ω(err).ShouldNot(HaveOccurred(), "note: we don't error")
-					Ω(failedWork.Stops).Should(ConsistOf(stoppingLRP))
-					Ω(failedWork.Stops).Should(ConsistOf(stoppingLRP))
+					Ω(failedWork.LRPStops).Should(ConsistOf(stoppingLRP))
 				})
 			})
 		})