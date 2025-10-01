0bfde0b8785feb2aba653be1b6985f587a4085f8
karthick udayakumar <karthick.udayakumar@broadcom.com>
2025-10-01 17:00:18 -0400

    add metron helper in file-server

--- src/code.cloudfoundry.org/fileserver/cmd/file-server/main_suite_test.go
+++ src/code.cloudfoundry.org/fileserver/cmd/file-server/main_suite_test.go
@@ -1,6 +1,9 @@
 package main_test
 
 import (
+	"code.cloudfoundry.org/bbs/test_helpers"
+	"code.cloudfoundry.org/diego-logging-client/testhelpers"
+	"code.cloudfoundry.org/go-loggregator/v9/rpc/loggregator_v2"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
@@ -8,7 +11,14 @@ import (
 	"testing"
 )
 
-var fileServerBinary string
+var (
+	fileServerBinary   string
+	testIngressServer  *testhelpers.TestIngressServer
+	metronIngressSetup *test_helpers.MetronIngressSetup
+
+	testMetricsChan   chan *loggregator_v2.Envelope
+	signalMetricsChan chan struct{}
+)
 
 func TestFileServer(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -24,7 +34,23 @@ var _ = SynchronizedBeforeSuite(func() []byte {
 
 })
 
+var _ = BeforeEach(func() {
+	var err error
+	metronIngressSetup, err = test_helpers.StartMetronIngress("fixtures")
+	Expect(err).NotTo(HaveOccurred())
+	testIngressServer = metronIngressSetup.Server
+	signalMetricsChan = metronIngressSetup.SignalMetricsChan
+	testMetricsChan = metronIngressSetup.TestMetricsChan
+
+})
+
 var _ = SynchronizedAfterSuite(func() {
 }, func() {
 	gexec.CleanupBuildArtifacts()
+})
+
+var _ = AfterEach(func() {
+	testIngressServer.Stop()
+	close(signalMetricsChan)
+
 })
